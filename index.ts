f31346a6a6c0f9859bc0e5e77f62cc7e6fce13c7
Anderson Pozo <imbander092098@gmail.com>
2019-11-24 00:15:48 -0500

    primer commit

---
+++ index.ts
@@ -0,0 +1,44 @@
+import  Server  from "./classes/server";
+import userRoutes from "./routes/usuario";
+import mongoose from 'mongoose';
+
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import fileUpload from 'express-fileupload';
+import postRoutes from "./routes/post";
+
+
+const server = new Server();
+
+// Midelware (body parser)
+server.app.use(bodyParser.urlencoded({extended: true}));
+server.app.use(bodyParser.json());
+
+
+//FileUpload
+server.app.use(fileUpload());
+
+
+// Configuracion de CORS
+server.app.use(cors({origin: true, credentials: true}))
+
+//Rutas de la aplicacion
+server.app.use('/user',userRoutes);
+server.app.use('/posts',postRoutes);
+
+//Conectar DB
+let uri = 'mongodb+srv://qrscanner:kIV5CknMU72FPsti@cluster0-gblzn.mongodb.net/scanner?retryWrites=true&w=majority' 
+
+mongoose.connect(uri,
+            { useNewUrlParser: true, useCreateIndex:true },(err)=>{
+            
+                if (err) throw err;
+                console.log('Base de datos online');               
+})
+
+
+
+//Levantar express 
+server.start(()=>{
+    console.log(`Servidor corriendo en puerto ${server.port}`);
+});
